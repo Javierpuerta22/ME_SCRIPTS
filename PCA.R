0b1964ccf497b236e243a384e7198f9e61eccded
javierpuerta22 <javier.puerta@estudiantat.upc.edu>
2022-10-07 11:38:36 +0200

    pca

--- PCA.R
+++ PCA.R
@@ -1,7 +1,7 @@
 # PCA 
 
-install.packages("FactoMineR")
-install.packages("factoextra")
+#install.packages("FactoMineR")
+#install.packages("factoextra")
 
 library(FactoMineR)
 library(factoextra)
@@ -12,4 +12,42 @@ clases <- lapply(train, class)      #lista con las clases de cada variable
 
 aux <- subset.data.frame(train, drop = FALSE, select = which(clases == "numeric" | clases == "integer"))
 
-pc <- PCA(aux, scale.unit = TRUE)
+aux2 <- subset.data.frame(train, drop = FALSE, select = which(clases == "character"))
+
+
+num = list()
+cat = list()
+
+
+
+for (i in length(clases)){
+  if (i == "character"){
+    num = c(num, i)
+  }
+  else{
+    cat = c(cat, i)
+  }
+}
+
+
+pc <- PCA(aux, scale.unit = TRUE, ncp = 7)
+
+a <- get_pca(pc)
+
+fviz_pca(pc)
+
+a$cor
+
+#---------------------------- porcentaje variables --------------------------------------------
+
+
+
+
+
+res.pca <- PCA(train, scale.unit = TRUE, quali.sup = c(1,3,4,5,6,7, 9,10,15,17,18), graph=FALSE)
+
+
+fviz_pca_ind(res.pca, habillage = 15,
+             addEllipses =TRUE, ellipse.level = 0.5) +
+  scale_color_brewer(palette="Dark2") +
+  theme_minimal()
