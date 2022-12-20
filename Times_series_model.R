e22afc5d03d5b631c160bfb0a7824c3bd0b70bfe
AdriSvm <adria.svm@gmail.com>
2022-12-20 17:31:37 +0100

    Time series model

--- Times_series_model.R
+++ Times_series_model.R
@@ -1,23 +1,24 @@
 
-# 
+# Convertim la sèrie temporal en un objecte temporal
 timesSeries <- ts(datosTS, start = c(1976, 1), frequency = 12)
 
-# Dibujamos el grafico de la serie temporal inicial
+# Dibuixem el gràfic de la sèrie inicial
 plot(timesSeries)
 
-# Como no se ve una real mierda, vamos hacer los correlogramas
+# Fem els correlogrames ja que no es veu molta cosa
 {acf(timesSeries, lag.max = 46*12)
 pacf(timesSeries, lag.max = 46*12)}
-#Las FAS(ACF) decreix lentament, per tant la sèrie no és estacionària.
-# Realizamos el logaritmo al ser no estacionaria en variancia
+#Las FAS(ACF) decreix lentament, per tant la sèrie no és estacionària
+# Realitzem el logaritme al no ser estacionària en variança
 #Com la sèrie no és estacionària, hem de fer el logaritme i al veure la Fas i la FAP
 # del logaritme, podem veure si es AR o MA
 log_ts <- log(timesSeries)
 plot(log_ts)
 {acf(log_ts, lag.max = 46*12)
   pacf(log_ts, lag.max = 46*12)}
+
 #La mitja dels valors al llarg del temps de la sèrie temporal no és constant, per tant, no és estacionària.
-# Realizamos una diferencia regular (d=1) debido a la no estacionariedad en media en la parte regular
+# Realitzem una diferència regular (d=1) a causa de la no estacionaritat en mitja
 log_ts_d1 <- diff(log_ts)
 plot(log_ts_d1)
 {acf(log_ts_d1, lag.max = 46*12)
@@ -27,19 +28,22 @@ pacf(log_ts_d1, lag.max = 46*12)}
 {acf(log_ts_d1, lag.max = 12)
   pacf(log_ts_d1, lag.max = 12)}
 
+
+# Model final ARIMA(0, 1, 2)(0, 0, 2)_12
 library(forecast)
-# ARIMA(0, 1, 2)(0, 0, 2)_12
 forecast::auto.arima(timesSeries)
 arima12 <- arima(timesSeries, order = c(0, 1, 2), seasonal = c(0, 0, 2))
 summary(arima12)
 library(lmtest)
 coeftest(arima12)
 
+#Validació del model
 residuales <- residuals(arima12)
 mean(residuales)
 var(residuales)
 checkresiduals(arima12)
-####Prediccions
+
+# Realitzem les prediccions amb un 60% de les mostres i veiem els resultats
 a <- length(timesSeries) * 0.6
 setenta <- datosTS[0:a]
 timeSeries70 <- ts(setenta,start=c(1976,1), frequency = 12)
